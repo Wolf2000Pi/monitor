91de7977af9180f8d4e86e089ad65d5b11c6dc9b
Wolf2000 <support@wolf2000.at>
2026-03-17 11:07:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -296,6 +296,16 @@ const server = http.createServer(async (req, res) => {
         res.end(data);
       }
     });
+  } else if (url === '/index.html') {
+    fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        res.end('Error loading index.html');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      }
+    });
   } else if (url === '/api/status') {
     const status = await checkAllServices();
     res.end(JSON.stringify(status));
